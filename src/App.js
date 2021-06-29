cc92dd6ff9e46a51bb1ded1e6aa5739959cb30bc
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-06-29 11:58:08 +0200

    Add files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import navBar from './Components/NavBar';
+
 function App() {
+  const [data, setData] = useState([]);
+  async function getUser() {
+    try {
+      const { response } = await axios.get('https://superheroapi.com/api/10151718746214987');
+      console.log(response);
+      setData(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    getUser();
+  }, []);
   return (
     <div className="App">
       Hello
+      <navBar />
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function navBar() {
+  return (
+    <div />
+  );
+}
+
+// navBar.propTypes = {
+// };
+
+export default navBar;
