d3edebdad4c91f6e1f0bde5435b124f033bd2317
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-06-30 21:05:39 +0200

    Add nothing usefull

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Form from './Components/Form';
@@ -5,33 +6,55 @@ import SuperCard from './Components/SuperCard';
 // import navBar from './Components/NavBar';
 function App() {
   const [data, setData] = useState([]);
+  const urlData = 'https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json';
   const fetchData = async () => {
     try {
-      const { data } = await axios.get(`https://www.superheroapi.com/api.php/10151718746214987/${Math.floor(Math.random() * 730)}`);
-      setData([data]);
+      // const newData = await axios.get(`https://www.superheroapi.com/api.php/10151718746214987/${Math.floor(Math.random() * 730)}`);
+      const { data } = await axios.get(urlData);
 
+      const superheroes = data.sort(() => Math.random() - 0.5);
+
+      const strippedSuperheroes = superheroes.map((superhero) => {
+        const {
+          name,
+          powerstats,
+          appearance: { gender, race },
+          biography: { fullName, firstAppearance, publisher },
+          work: { occupation },
+          images: { lg: image },
+        } = superhero;
+
+        const strippedSuperhero = {
+          name, powerstats, gender, race, fullName, firstAppearance, publisher, occupation, image,
+        };
+
+        return strippedSuperhero;
+      });
+      return strippedSuperheroes;
       // const { data2 } = await axios.get('https://www.superheroapi.com/api.php/10151718746214987/2');
       // setData([...data, data2]);
       // setData([data]);
-      // console.log([data]);
-      return data;
     } catch (error) {
       return error;
     }
   };
   useEffect(() => {
-    for (let i = 0; i < 5; i += 1) {
-      fetchData();
-    }
+    const fetchingData = async () => {
+      setData(await fetchData());
+    };
+
+    fetchingData();
+    // for (let i = 0; i < 5; i += 1) {
+    // fetchData().then((item) => setData(item));
+    // }
   }, []);
 
   return (
     <div className="App">
       <Form />
-      {console.log(data[0])}
       <div>
-        {data && <SuperCard data={data[0]} />}
-        {data && <SuperCard data={data[1]} />}
+        {console.log(data)}
+        <SuperCard data={data} />
       </div>
     </div>
   );
--- src/Components/SuperCard.js
+++ src/Components/SuperCard.js
@@ -1,31 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-const SuperCard = ({ data }) => (
-  <div className="card" style={{ width: '18rem' }}>
-    <img className="card-img-top" src={data.image.url} alt="Card" />
-    <div className="card-body">
-      <h5 className="card-title">{data.name}</h5>
-      <p className="card-text">{data.biography['full-name']}</p>
+const SuperCard = (props) => (
+  props.data.map((data) => (
+    <div className="card" style={{ width: '18rem' }} key="data.id">
+      <img className="card-img-top" src="ndhd" alt="Card" />
+      <div className="card-body">
+        <h5 className="card-title">{data}</h5>
+        <p className="card-text">jdkjdkljdkl</p>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Cras justo odio</li>
+        <li className="list-group-item">Dapibus ac facilisis in</li>
+        <li className="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div className="card-body">
+        <a href="#index" className="card-link">Card link</a>
+        <a href="#index" className="card-link">Another link</a>
+      </div>
     </div>
-    <ul className="list-group list-group-flush">
-      <li className="list-group-item">Cras justo odio</li>
-      <li className="list-group-item">Dapibus ac facilisis in</li>
-      <li className="list-group-item">Vestibulum at eros</li>
-    </ul>
-    <div className="card-body">
-      <a href="#index" className="card-link">Card link</a>
-      <a href="#index" className="card-link">Another link</a>
-    </div>
-  </div>
+  ))
 );
-SuperCard.propTypes = {
-//   data: PropTypes.shape({
-//     name: PropTypes.string.isRequired,x`x
-//     image: PropTypes.shape({
-//       url: PropTypes.string.isRequired,
-//     }).isRequired,
-//   }).isRequired,
-  data: PropTypes.objectOf(PropTypes.object).isRequired,
-};
+// SuperCard.propTypes = {
+// // //   data: PropTypes.shape({
+// // //     name: PropTypes.string.isRequired,x`x
+// // //     image: PropTypes.shape({
+// // //       url: PropTypes.string.isRequired,
+// // //     }).isRequired,
+// // //   }).isRequired,
+//   data: PropTypes.objectOf(PropTypes.object).isRequired,
+
+// };
 export default SuperCard;
